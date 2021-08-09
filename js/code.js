d330f0b710657693f3204be5a89c18fed6089313
luckylovs <77058551+luckylovs@users.noreply.github.com>
2021-08-09 13:23:33 +0530

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE"
+const accessCode2 = "FUNCTION"
+const accessCode3 = "DATABASE"
+
+function clues(){
+    fill("white")
+    textSize(20)
+    text("R E V B A I L A",100,50);
+    textSize(15)
+    fill("lightBlue")
+    text("Hint: Always Changing Not Constant!!",100,70)
+
+
+    fill("white")
+    textSize(20)
+    text("C U T N I F O N",700,150);
+    textSize(15)
+    fill("lightBlue")
+    text("Hint: Perform A Particular Task!!",700,170)
+
+    fill("white")
+    textSize(20)
+    text("A T E D A S B A ",100,310);
+    textSize(15)
+    fill("lightBlue")
+    text("Hint: Stores All Information!!",100,330)
+
+}  
